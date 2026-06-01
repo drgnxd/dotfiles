301a066487bf023b3b621bbb122e901f8bc59853
drgnxd <148034321+drgnxd@users.noreply.github.com>
2026-06-01 12:00:00 +0900

    fix(ssh): resolve identitiesonly issue with agent by adding identityfile

--- home/modules/ssh.nix
+++ home/modules/ssh.nix
@@ -23,6 +23,7 @@
       };
       "github.com" = {
         HostName = "github.com";
+        IdentityFile = "~/.ssh/id_proton_github.pub";
         IdentitiesOnly = true;
         User = "git";
       };
