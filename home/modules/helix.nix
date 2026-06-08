2f3402781adc3069e6680cabdb192c8a68fd8dc4
drgnxd <148034321+drgnxd@users.noreply.github.com>
2026-06-08 12:00:00 +0900

    refactor(helix): derive nixd config names from identity

--- home/modules/helix.nix
+++ home/modules/helix.nix
@@ -1,9 +1,22 @@
-_:
+{
+  user,
+  hostname,
+  linuxHostname,
+  ...
+}:
 
+let
+  languages_toml = builtins.readFile ../../dot_config/helix/languages.toml;
+  languages_toml_substituted =
+    builtins.replaceStrings
+      [ "darwinConfigurations.darwin" ''homeConfigurations."user@linux"'' ]
+      [ "darwinConfigurations.${hostname}" ''homeConfigurations."${user}@${linuxHostname}"'' ]
+      languages_toml;
+in
 {
   xdg.configFile = {
     "helix/config.toml".source = ../../dot_config/helix/config.toml;
-    "helix/languages.toml".source = ../../dot_config/helix/languages.toml;
+    "helix/languages.toml".text = languages_toml_substituted;
     "helix/themes/solarized_dark_transparent.toml".source =
       ../../dot_config/helix/themes/solarized_dark_transparent.toml;
   };
