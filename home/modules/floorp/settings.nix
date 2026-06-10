e0db5adc5705d155eb001938ab67a95e4d501691
drgnxd <148034321+drgnxd@users.noreply.github.com>
2026-06-10 16:41:43 +0900

    feat(floorp): add shared platform-independent settings and chrome assets

---
+++ home/modules/floorp/settings.nix
@@ -0,0 +1,7 @@
+{
+  # >>> shared-platform-independent (do not add isDarwin/isLinux branching below)
+  "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+  "browser.toolbars.bookmarks.visibility" = "never";
+  "browser.startup.page" = 3;
+  # <<< shared-platform-independent
+}
