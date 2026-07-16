4ef08440c5f44f4435bc2bba1fbd30ae4cc05dca
drgnxd <148034321+drgnxd@users.noreply.github.com>
2026-07-17 03:16:32 +0900

    fix(atuin): filter internal Nushell commands

--- home/modules/atuin.nix
+++ home/modules/atuin.nix
@@ -10,6 +10,9 @@
       history_filter = [
         "^SecretCommand"
         "^base64"
+        "^# [0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\\n"
+        "^nu_abbr_insert_space$"
+        "^nu_abbr_submit$"
       ];
       # Atuin defaults to the legacy ~/.atuin/logs path.
       logs.dir = "${config.xdg.stateHome}/atuin/logs";
