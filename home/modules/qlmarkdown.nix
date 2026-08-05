74e03d131d4db2116f3ef4f590caf713cc4c6080
drgnxd <148034321+drgnxd@users.noreply.github.com>
2026-08-05 22:00:17 +0900

    feat(qlmarkdown): enable subscript and superscript

--- home/modules/qlmarkdown.nix
+++ home/modules/qlmarkdown.nix
@@ -71,6 +71,10 @@ in
     if [ -f "${settings}" ]; then
       /usr/libexec/PlistBuddy -c 'Set :customCSS terminal-font.css' "${settings}"
       /usr/libexec/PlistBuddy -c 'Set :customCSSOverride false' "${settings}"
+      /usr/libexec/PlistBuddy -c 'Set :footnotesOption false' "${settings}"
+      /usr/libexec/PlistBuddy -c 'Set :strikethroughExtension 2' "${settings}"
+      /usr/libexec/PlistBuddy -c 'Set :subExtension true' "${settings}"
+      /usr/libexec/PlistBuddy -c 'Set :supExtension true' "${settings}"
       /usr/bin/qlmanage -r cache >/dev/null
     fi
   '';
